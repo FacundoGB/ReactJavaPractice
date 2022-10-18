cc11b647237bf34f6fa08006a92abd879e2e21c5
github_FacundoGB <bardifacundo@gmail.com>
2022-10-18 20:06:14 -0300

    added hardcoded data and buttons

--- src/pages/client/ClientList.tsx
+++ src/pages/client/ClientList.tsx
@@ -14,8 +14,8 @@ import {
     IonTitle,
     IonToolbar
 } from '@ionic/react';
-import { add } from 'ionicons/icons';
-import {useParams} from 'react-router';
+import {add, pencil, close} from 'ionicons/icons';
+import {Redirect, useParams} from 'react-router';
 import ExploreContainer from '../../components/ExploreContainer';
 
 
@@ -46,137 +46,34 @@ const ClientList: React.FC = () => {
                     <IonTitle>Client Management</IonTitle>
                     <IonItem>
                         <IonButton color="primary" fill='solid' slot='end' size='default'>
-                          <IonIcon icon={add} />
-                          Add Client
+                            <IonIcon icon={add}/>
+                            Add Client
                         </IonButton>
 
                     </IonItem>
                     <IonGrid className='table'>
                         <IonRow> {/* in style={} we can add an object that represents a css code */}
-                            <IonCol>ion-col</IonCol>
-                            <IonCol>ion-col</IonCol>
-                            <IonCol>ion-col</IonCol>
-                            <IonCol>ion-col</IonCol>
+                            <IonCol>Name</IonCol>
+                            <IonCol>Email</IonCol>
+                            <IonCol>Phone</IonCol>
+                            <IonCol>Adress</IonCol>
+                            <IonCol>Actions</IonCol>
                         </IonRow>
 
                         <IonRow>
-                            <IonCol size="6">ion-col size="6"</IonCol>
-                            <IonCol>ion-col</IonCol>
-                            <IonCol>ion-col</IonCol>
-                        </IonRow>
-
-                        <IonRow>
-                            <IonCol size="3">ion-col size="3"</IonCol>
-                            <IonCol>ion-col</IonCol>
-                            <IonCol size="3">ion-col size="3"</IonCol>
-                        </IonRow>
-
-                        <IonRow>
-                            <IonCol size="3">ion-col size="3"</IonCol>
-                            <IonCol size="3" offset="3">
-                                ion-col size="3" offset="3"
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow>
-                            <IonCol>ion-col</IonCol>
+                            <IonCol>Facundo Bardi</IonCol>
+                            <IonCol>mail@mail.com</IonCol>
+                            <IonCol>111222111</IonCol>
+                            <IonCol>direccion 232</IonCol>
                             <IonCol>
-                                ion-col
-                                <br/>#
-                            </IonCol>
-                            <IonCol>
-                                ion-col
-                                <br/>#
-                                <br/>#
-                            </IonCol>
-                            <IonCol>
-                                ion-col
-                                <br/>#
-                                <br/>#
-                                <br/>#
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow>
-                            <IonCol className="ion-align-self-start">ion-col start</IonCol>
-                            <IonCol className="ion-align-self-center">ion-col center</IonCol>
-                            <IonCol className="ion-align-self-end">ion-col end</IonCol>
-                            <IonCol>
-                                ion-col
-                                <br/>#
-                                <br/>#
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow className="ion-align-items-start">
-                            <IonCol>start ion-col</IonCol>
-                            <IonCol>start ion-col</IonCol>
-                            <IonCol className="ion-align-self-end">start ion-col end</IonCol>
-                            <IonCol>
-                                ion-col
-                                <br/>#
-                                <br/>#
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow className="ion-align-items-center">
-                            <IonCol>center ion-col</IonCol>
-                            <IonCol>center ion-col</IonCol>
-                            <IonCol>center ion-col</IonCol>
-                            <IonCol>
-                                ion-col
-                                <br/>#
-                                <br/>#
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow className="ion-align-items-end">
-                            <IonCol>end ion-col</IonCol>
-                            <IonCol className="ion-align-self-start">end ion-col start</IonCol>
-                            <IonCol>end ion-col</IonCol>
-                            <IonCol>
-                                ion-col
-                                <br/>#
-                                <br/>#
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow>
-                            <IonCol size="12" size-sm>
-                                ion-col size="12" size-sm
-                            </IonCol>
-                            <IonCol size="12" size-sm>
-                                ion-col size="12" size-sm
-                            </IonCol>
-                            <IonCol size="12" size-sm>
-                                ion-col size="12" size-sm
-                            </IonCol>
-                            <IonCol size="12" size-sm>
-                                ion-col size="12" size-sm
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow>
-                            <IonCol size="12" size-md>
-                                ion-col size="12" size-md
-                            </IonCol>
-                            <IonCol size="12" size-md>
-                                ion-col size="12" size-md
-                            </IonCol>
-                            <IonCol size="12" size-md>
-                                ion-col size="12" size-md
-                            </IonCol>
-                            <IonCol size="12" size-md>
-                                ion-col size="12" size-md
-                            </IonCol>
-                        </IonRow>
-
-                        <IonRow>
-                            <IonCol size="6" size-lg offset="3">
-                                ion-col size="6" size-lg offset="3"
-                            </IonCol>
-                            <IonCol size="3" size-lg>
-                                ion-col size="3" size-lg
+                              <IonButton color='primary' fill='clear' >
+                                <IonIcon icon={pencil}/>
+                                
+                              </IonButton>
+                              <IonButton color='danger' fill='clear' >
+                                <IonIcon icon={close}/>
+                                
+                              </IonButton>
                             </IonCol>
                         </IonRow>
                     </IonGrid>
