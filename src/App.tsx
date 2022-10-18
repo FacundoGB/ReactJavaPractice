43d8cdc2e557bb93b38f07997ccc3f30587a4bd7
github_FacundoGB <bardifacundo@gmail.com>
2022-10-18 17:09:06 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,7 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import ClientList from './pages/client/ClientList';
 
 setupIonicReact();
 
@@ -33,11 +34,13 @@ const App: React.FC = () => {
           <Menu />
           <IonRouterOutlet id="main">
             <Route path="/" exact={true}>
-              <Redirect to="/page/Inbox" />
+              <Redirect to="/page/clients" />
             </Route>
-            <Route path="/page/:name" exact={true}>
-              <Page />
+
+            <Route path="/page/clients" exact={true}>
+              <ClientList />
             </Route>
+
           </IonRouterOutlet>
         </IonSplitPane>
       </IonReactRouter>
