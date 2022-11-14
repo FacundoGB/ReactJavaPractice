c6e158b677aac8580d66aea9bc5004fdc8203255
github_FacundoGB <bardifacundo@gmail.com>
2022-11-14 18:01:57 -0300

    copied structure of client for new object. refractored files for it.

---
+++ src/pages/employee/EmployeeEdit.tsx
@@ -0,0 +1,123 @@
+import {
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonMenuButton,
+    IonPage,
+    IonRow,
+    IonTitle,
+    IonToolbar,
+    IonLabel,
+    IonInput
+} from '@ionic/react';
+import { add, pencil, close, checkmark } from 'ionicons/icons';
+import { useEffect, useState } from 'react';
+import { Redirect, useHistory, useParams } from 'react-router';
+import {saveEmployee, searchEmployeeById } from './EmployeeApi';
+import EmployeeInterface from './EmployeeInterface';
+
+
+
+const EmployeeEdit: React.FC = () => {
+
+    const { name, id } = useParams<{
+        name: string;
+        id: string;
+    }>();
+
+    const [employee, setEmployee] = useState<EmployeeInterface>({});
+
+    useEffect(() => {
+        search();
+    }, []);
+    const history = useHistory();
+
+    const search = () => {
+        if(id !== 'new') {
+            let result = searchEmployeeById(id);
+            setEmployee(result);
+        }
+    }
+
+    const save = () => {
+        debugger;   
+        saveEmployee(employee);
+        history.push('/page/employees')
+    }
+    
+
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>{name}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large"></IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonCard>
+                    <IonTitle>{id === 'new' ? 'Set New Employee' : 'Edit Employee'}</IonTitle>
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="stacked">Name</IonLabel>
+                                <IonInput onIonChange={e => employee.firstname = String(e.detail.value)} value={employee.firstname}></IonInput>
+                            </IonItem>
+                        </IonCol>
+
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="stacked">Surname</IonLabel>
+                                <IonInput onIonChange={e => employee.surname = String(e.detail.value)} value={employee.surname}></IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="stacked">Email</IonLabel>
+                                <IonInput onIonChange={e => employee.email = String(e.detail.value)} value={employee.email}></IonInput>
+                            </IonItem>
+                        </IonCol>
+
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="stacked">Adress</IonLabel>
+                                <IonInput onIonChange={e => employee.address = String(e.detail.value)} value={employee.address}></IonInput>
+                            </IonItem>
+                        </IonCol>
+                        <IonCol>
+                            <IonItem>
+                                <IonLabel position="stacked">Phone</IonLabel>
+                                <IonInput onIonChange={e => employee.phone = String(e.detail.value)} value={employee.phone}></IonInput>
+                            </IonItem>
+                        </IonCol>
+                    </IonRow>
+                    <IonItem>
+                        <IonButton onClick={save} color="primary" fill='solid' slot='end' size='default'>
+                            <IonIcon icon={checkmark} />
+                            Save Changes
+                        </IonButton>
+                    </IonItem>
+                </IonCard>
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default EmployeeEdit;
---
+++ src/pages/employee/EmployeeList.tsx
@@ -0,0 +1,120 @@
+import {
+    IonButton,
+    IonButtons,
+    IonCard,
+    IonCol,
+    IonContent,
+    IonGrid,
+    IonHeader,
+    IonIcon,
+    IonItem,
+    IonMenuButton,
+    IonPage,
+    IonRow,
+    IonTitle,
+    IonToolbar
+} from '@ionic/react';
+import {add, pencil, close} from 'ionicons/icons';
+import { useEffect, useState } from 'react';
+import {Redirect, useHistory, useParams} from 'react-router';
+import { removeEmployee, saveEmployee, searchEmployee} from './EmployeeApi';
+import EmployeeInterface from './EmployeeInterface';
+
+const EmployeeList: React.FC = () => {
+
+    const {name} = useParams < {
+        name: string;
+    } > ();
+
+    
+   const [employees, setEmployees] = useState<EmployeeInterface[]>([]); 
+   const history = useHistory();
+
+  useEffect(() => {
+    /**With search() it will call the api and fill with data the grid */
+    search();
+  }, [history.location.pathname]); 
+
+    const search = () => {
+      let result = searchEmployee();
+      setEmployees(result);
+    }
+
+    const remove = (id: string) => {
+         removeEmployee(id);
+        search();
+    }
+
+    const editEmployee = (id:string) => {
+        // this adds rout and changes page 
+        history.push('/page/employee/' + id);
+    }
+    
+    const addEmployee = () => {
+        // this adds rout and changes page 
+        history.push('/page/employee/new');
+
+    }
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton/>
+                    </IonButtons>
+                    <IonTitle>{name}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large"></IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonCard>
+                    <IonTitle>Employee Management</IonTitle>
+                    <IonItem>
+                        <IonButton onClick={addEmployee} color="primary" fill='solid' slot='end' size='default'>
+                            <IonIcon icon={add}/>
+                            Add Employee
+                        </IonButton>
+
+                    </IonItem>
+                    <IonGrid className='table'>
+                        <IonRow> {/* in style={} we can add an object that represents a css code */}
+                            <IonCol>Name</IonCol>
+                            <IonCol>Email</IonCol>
+                            <IonCol>Phone</IonCol>
+                            <IonCol>Adress</IonCol>
+                            <IonCol>Actions</IonCol>
+                        </IonRow>
+
+                        {employees.map((employee:any)=>
+                        /*for each client it will return a row */
+                        <IonRow>
+                            <IonCol>{employee.firstname} {employee.surname}</IonCol>
+                            <IonCol>{employee.email}</IonCol>
+                            <IonCol>{employee.phone}</IonCol>
+                            <IonCol>{employee.address}</IonCol>
+                            <IonCol>
+                              <IonButton onClick={() => editEmployee(String(employee.id))} color='primary' fill='clear' >
+                                <IonIcon icon={pencil}/>
+                                
+                              </IonButton>
+                              <IonButton onClick={() => remove(String(employee.id))} color='danger' fill='clear' >
+                                <IonIcon icon={close}/>
+                                
+                              </IonButton>
+                            </IonCol>
+                        </IonRow>
+                        )}
+                    </IonGrid>
+                </IonCard>   
+            </IonContent>
+        </IonPage>
+    );
+};
+
+export default EmployeeList;
