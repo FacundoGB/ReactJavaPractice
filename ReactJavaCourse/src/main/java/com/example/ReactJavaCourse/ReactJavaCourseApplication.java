cff84d8bf8bba9899332961d2076203eacf6ed78
github_FacundoGB <bardifacundo@gmail.com>
2022-11-15 16:07:30 -0300

    Created backend in java

---
+++ ReactJavaCourse/src/main/java/com/example/ReactJavaCourse/ReactJavaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReactJavaCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactJavaCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactJavaCourseApplication.class, args);
+	}
+
+}
