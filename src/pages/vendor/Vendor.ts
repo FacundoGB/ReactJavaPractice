8f90c4399f4fc993af529e4213594d761875fe8c
github_FacundoGB <bardifacundo@gmail.com>
2022-11-18 18:20:52 -0300

    modified vendorInterface to just vendor. works as model of object

---
+++ src/pages/vendor/Vendor.ts
@@ -0,0 +1,15 @@
+/**
+ * A Model of class that allows us to mantain the type structure of our object
+ * There are two types of models in typescript: Classes and Interface, both define the structure of the model but Classes allow methods.
+ * We are using Typescript, if we create Classes it will generate a JS with classes, if we use Interfaces it wont, the app wil weight less.
+ */
+
+export default interface Vendor {
+    id?: string;
+    name?: string;
+    email?: string;
+    phone?: string;
+    address?: string;
+    web?: string;
+    contact?: string;
+}
