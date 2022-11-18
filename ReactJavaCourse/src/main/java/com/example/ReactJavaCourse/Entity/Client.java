50555b3775f011569f370a64683e0ea18ddede78
github_FacundoGB <bardifacundo@gmail.com>
2022-11-18 16:03:40 -0300

    made inconsecuential changes to client entity

--- ReactJavaCourse/src/main/java/com/example/ReactJavaCourse/Entity/Client.java
+++ ReactJavaCourse/src/main/java/com/example/ReactJavaCourse/Entity/Client.java
@@ -1,5 +1,24 @@
 package com.example.ReactJavaCourse.Entity;
 
+import lombok.*;
+
+import javax.persistence.*;
+
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+@ToString @EqualsAndHashCode
+@Entity
+@Table(name="clients")
 public class Client {
-    private 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idclients;
+
+    private String firstname;
+    private String surname;
+    private String email;
+    private String phone;
+    private String address;
+
 }
